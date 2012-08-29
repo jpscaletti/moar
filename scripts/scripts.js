72f1e0d6288595b8d61b4c9d2dd792f4db87ca82
Jay-P <juanpablo@lucumalabs.com>
2012-08-29 14:31:00 -0500

    Update doc

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,19 +1,4 @@
 (function($){
 "use strict";
 
-function setViewport() {
-  if (wwidth > 631 && wwidth < 899){
-    return $("html,body").animate({
-      scrollLeft: rwidth
-    }, 700), !1;
-  }
-}
-
-$(window).load(function () {
-  setViewport();
-});
-
-var rwidth = $("#river").width();
-var wwidth = $(window).width();
-
 }(jQuery));
