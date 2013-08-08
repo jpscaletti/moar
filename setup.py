5c7ef5a6cda7d6828129e70651072be1c489b320
Juan-Pablo Scaletti <juanpablo@lucumalabs.com>
2013-08-08 17:36:44 -0500

    Update the setup.py

--- setup.py
+++ setup.py
@@ -2,31 +2,31 @@
 import io
 import os
 import re
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 
 PACKAGE = 'moar'
-THIS_DIR = os.path.dirname(__file__).rstrip('/')
 
 
 def get_path(*args):
-    return os.path.join(THIS_DIR, *args)
+    return os.path.join(os.path.dirname(__file__), *args)
 
 
 def read_from(filepath):
-    with io.open(filepath, 'rt', encoding='utf-8') as f:
-        source = f.read()
-    return source
+    with io.open(filepath, 'rt', encoding='utf8') as f:
+        return f.read()
 
 
 def get_version():
     data = read_from(get_path(PACKAGE, '__init__.py'))
-    version = re.search(r"__version__\s*=\s*'([^']+)'", data).group(1)
-    return version
+    version = re.search(r"__version__\s*=\s*u?'([^']+)'", data).group(1)
+    return str(version)
 
 
-def find_package_data(root, include_files=None):
-    include_files = include_files or ['.gitignore',]
+def find_package_data(root, include_files=('.gitignore', )):
     files = []
     src_root = get_path(root).rstrip('/') + '/'
     for dirpath, subdirs, filenames in os.walk(src_root):
@@ -50,34 +50,41 @@ def find_packages_data(*roots):
     return dict([(root, find_package_data(root)) for root in roots])
 
 
-def get_requirements():
-    data = read_from(get_path('requirements.txt'))
+def get_description():
+    data = read_from(get_path(PACKAGE, '__init__.py'))
+    desc = re.search('"""(.+)"""', data, re.DOTALL).group(1)
+    return desc.strip()
+
+
+def get_requirements(filename='requirements.txt'):
+    data = read_from(get_path(filename))
     lines = map(lambda s: s.strip(), data.splitlines())
     return [l for l in lines if l and not l.startswith('#')]
 
 
 setup(
-    name = 'Moar',
-    version = get_version(),
-    author = 'Juan-Pablo Scaletti',
-    author_email = 'juanpablo@lucumalabs.com',
-    packages = [PACKAGE],
-    package_data = find_packages_data(PACKAGE, 'tests'),
-    zip_safe = False,
-    url = 'http://github.com/lucuma/Moar',
-    license = 'MIT license (http://www.opensource.org/licenses/mit-license.php)',
-    description = 'Easy thumbnails for everyone.',
-    long_description = read_from(get_path('README.rst')),
-    install_requires = get_requirements(),
-    classifiers = [
+    name='Moar',
+    version=get_version(),
+    author='Juan-Pablo Scaletti',
+    author_email='juanpablo@lucumalabs.com',
+    packages=[PACKAGE],
+    package_data=find_packages_data(PACKAGE, 'tests'),
+    zip_safe=False,
+    url='http://github.com/lucuma/Moar',
+    license='MIT license (http://www.opensource.org/licenses/mit-license.php)',
+    description='Easy thumbnails for everyone',
+    long_description=get_description(),
+    install_requires=get_requirements(),
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
-    ],
-    test_suite = '__main__.run_tests',
+    ]
 )
